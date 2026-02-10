428c3aa28de690aa0b8c6100f936e62d9b5995db
Udara Wickramarathne <bimsaraudara25@gmail.com>
2026-02-10 14:30:00 +0530

    Add database models and face recognition model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,15 @@
-# Models Package
+"""
+Models Package
+MongoDB document schemas and database connection
+"""
+from app.models.database import get_database, connect_to_mongo, close_mongo_connection
+from app.models.face_model import FaceDocument, MerchantFaceDocument, VerificationLog
+
+__all__ = [
+    "get_database",
+    "connect_to_mongo",
+    "close_mongo_connection",
+    "FaceDocument",
+    "MerchantFaceDocument",
+    "VerificationLog"
+]
